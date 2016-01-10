954e20df6c49f16ef49d73f4511b3a3f0df7d9ab
jijoungsoo <jijoungsoo@gmail.com>
2016-01-11 08:54:45 +0900

    키움 open api  winform

---
+++ OpenApi/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace OpenApi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axKHOpenAPI1 = new AxKHOpenAPILib.AxKHOpenAPI();
+            this.lst에러 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lst일반 = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lst조회 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lst실시간 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.로그인버튼 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axKHOpenAPI1
+            // 
+            this.axKHOpenAPI1.Enabled = true;
+            this.axKHOpenAPI1.Location = new System.Drawing.Point(1151, 0);
+            this.axKHOpenAPI1.Name = "axKHOpenAPI1";
+            this.axKHOpenAPI1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axKHOpenAPI1.OcxState")));
+            this.axKHOpenAPI1.Size = new System.Drawing.Size(27, 24);
+            this.axKHOpenAPI1.TabIndex = 0;
+            this.axKHOpenAPI1.OnReceiveTrData += new AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEventHandler(this.axKHOpenAPI_OnReceiveTrData);
+            this.axKHOpenAPI1.OnReceiveRealData += new AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveRealDataEventHandler(this.axKHOpenAPI_OnReceiveRealData);
+            this.axKHOpenAPI1.OnReceiveMsg += new AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveMsgEventHandler(this.axKHOpenAPI_OnReceiveMsg);
+            this.axKHOpenAPI1.OnReceiveChejanData += new AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveChejanDataEventHandler(this.axKHOpenAPI_OnReceiveChejanData);
+            this.axKHOpenAPI1.OnEventConnect += new AxKHOpenAPILib._DKHOpenAPIEvents_OnEventConnectEventHandler(this.axKHOpenAPI_OnEventConnect);
+            this.axKHOpenAPI1.OnReceiveRealCondition += new AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveRealConditionEventHandler(this.axKHOpenAPI_OnReceiveRealCondition);
+            this.axKHOpenAPI1.OnReceiveTrCondition += new AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrConditionEventHandler(this.axKHOpenAPI_OnReceiveTrCondition);
+            this.axKHOpenAPI1.OnReceiveConditionVer += new AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveConditionVerEventHandler(this.axKHOpenAPI_OnReceiveConditionVer);
+            // 
+            // lst에러
+            // 
+            this.lst에러.FormattingEnabled = true;
+            this.lst에러.ItemHeight = 12;
+            this.lst에러.Location = new System.Drawing.Point(113, 230);
+            this.lst에러.Name = "lst에러";
+            this.lst에러.Size = new System.Drawing.Size(693, 88);
+            this.lst에러.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "lst에러";
+            // 
+            // lst일반
+            // 
+            this.lst일반.FormattingEnabled = true;
+            this.lst일반.ItemHeight = 12;
+            this.lst일반.Location = new System.Drawing.Point(113, 129);
+            this.lst일반.Name = "lst일반";
+            this.lst일반.Size = new System.Drawing.Size(693, 76);
+            this.lst일반.TabIndex = 4;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1178, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "lst일반";
+            // 
+            // lst조회
+            // 
+            this.lst조회.FormattingEnabled = true;
+            this.lst조회.ItemHeight = 12;
+            this.lst조회.Location = new System.Drawing.Point(113, 27);
+            this.lst조회.Name = "lst조회";
+            this.lst조회.Size = new System.Drawing.Size(693, 76);
+            this.lst조회.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(111, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "lst조회";
+            // 
+            // lst실시간
+            // 
+            this.lst실시간.FormattingEnabled = true;
+            this.lst실시간.ItemHeight = 12;
+            this.lst실시간.Location = new System.Drawing.Point(113, 348);
+            this.lst실시간.Name = "lst실시간";
+            this.lst실시간.Size = new System.Drawing.Size(693, 124);
+            this.lst실시간.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(111, 331);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "lst실시간";
+            // 
+            // 로그인버튼
+            // 
+            this.로그인버튼.Location = new System.Drawing.Point(825, 27);
+            this.로그인버튼.Name = "로그인버튼";
+            this.로그인버튼.Size = new System.Drawing.Size(75, 23);
+            this.로그인버튼.TabIndex = 1;
+            this.로그인버튼.Text = "로그인";
+            this.로그인버튼.UseVisualStyleBackColor = true;
+            this.로그인버튼.Click += new System.EventHandler(this.로그인_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1178, 538);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lst실시간);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lst조회);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lst일반);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lst에러);
+            this.Controls.Add(this.로그인버튼);
+            this.Controls.Add(this.axKHOpenAPI1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxKHOpenAPILib.AxKHOpenAPI axKHOpenAPI1;
+        private System.Windows.Forms.ListBox lst에러;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lst일반;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lst조회;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lst실시간;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button 로그인버튼;
+    }
+}
+
---
+++ OpenApi/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KiwoomCode;
+
+namespace OpenApi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 로그인_Click(object sender, EventArgs e)
+        {
+            if (axKHOpenAPI1.CommConnect() == 0)
+            {
+                Console.WriteLine("로그인");
+            }
+        }
+
+
+
+        private void axKHOpenAPI_OnReceiveTrData(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e)
+        {
+
+            if (e.sRQName == "주식주문")
+            {
+                string s원주문번호 = axKHOpenAPI1.GetCommData(e.sTrCode, "", 0, "").Trim();
+
+                long n원주문번호 = 0;
+                bool canConvert = long.TryParse(s원주문번호, out n원주문번호);
+
+                if (canConvert == true)
+                Logger(Log.에러, s원주문번호);
+                else
+                    Logger(Log.에러, "잘못된 원주문번호 입니다");
+
+            }
+            // OPT1001 : 주식기본정보
+            else if (e.sRQName == "주식기본정보")
+            {
+                int nCnt = axKHOpenAPI1.GetRepeatCnt(e.sTrCode, e.sRQName);
+
+                for (int i = 0; i < nCnt; i++)
+                {
+                    Logger(Log.조회, "{0} | 현재가:{1:N0} | 등락율:{2} | 거래량:{3:N0} ",
+                        axKHOpenAPI1.CommGetData(e.sTrCode, "", e.sRQName, i, "종목명").Trim(),
+                        Int32.Parse(axKHOpenAPI1.CommGetData(e.sTrCode, "", e.sRQName, i, "현재가").Trim()),
+                        axKHOpenAPI1.CommGetData(e.sTrCode, "", e.sRQName, i, "등락율").Trim(),
+                        Int32.Parse(axKHOpenAPI1.CommGetData(e.sTrCode, "", e.sRQName, i, "거래량").Trim()));
+                }
+            }
+            // OPT10081 : 주식일봉차트조회
+            else if (e.sRQName == "주식일봉차트조회")
+            {
+                int nCnt = axKHOpenAPI1.GetRepeatCnt(e.sTrCode, e.sRQName);
+
+                for (int i = 0; i < nCnt; i++)
+                {
+                    Logger(Log.조회, "{0} | 현재가:{1:N0} | 거래량:{2:N0} | 시가:{3:N0} | 고가:{4:N0} | 저가:{5:N0} ",
+                        axKHOpenAPI1.CommGetData(e.sTrCode, "", e.sRQName, i, "일자").Trim(),
+                        Int32.Parse(axKHOpenAPI1.CommGetData(e.sTrCode, "", e.sRQName, i, "현재가").Trim()),
+                        Int32.Parse(axKHOpenAPI1.CommGetData(e.sTrCode, "", e.sRQName, i, "거래량").Trim()),
+                        Int32.Parse(axKHOpenAPI1.CommGetData(e.sTrCode, "", e.sRQName, i, "시가").Trim()),
+                        Int32.Parse(axKHOpenAPI1.CommGetData(e.sTrCode, "", e.sRQName, i, "고가").Trim()),
+                        Int32.Parse(axKHOpenAPI1.CommGetData(e.sTrCode, "", e.sRQName, i, "저가").Trim()));
+                }
+            }
+
+        }
+
+        private void axKHOpenAPI_OnEventConnect(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnEventConnectEvent e)
+        {
+            if (Error.IsError(e.nErrCode))
+            {
+                Logger(Log.일반, "[로그인 처리결과] " + Error.GetErrorMessage());
+            }
+            else
+            {
+                Logger(Log.에러, "[로그인 처리결과] " + Error.GetErrorMessage());
+            }
+        }
+
+        private void axKHOpenAPI_OnReceiveChejanData(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveChejanDataEvent e)
+        {
+            if (e.sGubun == "0")
+            {
+                Logger(Log.실시간, "구분 : 주문체결통보");
+                Logger(Log.실시간, "주문/체결시간 : " + axKHOpenAPI1.GetChejanData(908));
+                Logger(Log.실시간, "종목명 : " + axKHOpenAPI1.GetChejanData(302));
+                Logger(Log.실시간, "주문수량 : " + axKHOpenAPI1.GetChejanData(900));
+                Logger(Log.실시간, "주문가격 : " + axKHOpenAPI1.GetChejanData(901));
+                Logger(Log.실시간, "체결수량 : " + axKHOpenAPI1.GetChejanData(911));
+                Logger(Log.실시간, "체결가격 : " + axKHOpenAPI1.GetChejanData(910));
+                Logger(Log.실시간, "=======================================");
+            }
+            else if (e.sGubun == "1")
+            {
+                Logger(Log.실시간, "구분 : 잔고통보");
+            }
+            else if (e.sGubun == "3")
+            {
+                Logger(Log.실시간, "구분 : 특이신호");
+            }
+
+        }
+
+        private void axKHOpenAPI_OnReceiveMsg(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveMsgEvent e)
+        {
+            Logger(Log.조회, "===================================================");
+            Logger(Log.조회, "화면번호:{0} | RQName:{1} | TRCode:{2} | 메세지:{3}", e.sScrNo, e.sRQName, e.sTrCode, e.sMsg);
+        }
+
+        private void axKHOpenAPI_OnReceiveRealData(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveRealDataEvent e)
+        {
+            Logger(Log.실시간, "종목코드 : {0} | RealType : {1} | RealData : {2}",
+                e.sRealKey, e.sRealType, e.sRealData);
+
+            if (e.sRealType == "주식시세")
+            {
+                Logger(Log.실시간, "종목코드 : {0} | 현재가 : {1:C} | 등락율 : {2} | 누적거래량 : {3:N0} ",
+                        e.sRealKey,
+                        Int32.Parse(axKHOpenAPI1.GetCommRealData(e.sRealType, 10).Trim()),
+                        axKHOpenAPI1.GetCommRealData(e.sRealType, 12).Trim(),
+                        Int32.Parse(axKHOpenAPI1.GetCommRealData(e.sRealType, 13).Trim()));
+            }
+
+        }
+
+        // 로그를 출력합니다.
+        public void Logger(Log type, string format, params Object[] args)
+        {
+            string message = String.Format(format, args);
+
+            switch (type)
+            {
+                case Log.조회:
+                    lst조회.Items.Add(message);
+                    lst조회.SelectedIndex = lst조회.Items.Count - 1;
+                    break;
+                case Log.에러:
+                    lst에러.Items.Add(message);
+                    lst에러.SelectedIndex = lst에러.Items.Count - 1;
+                    break;
+                case Log.일반:
+                    lst일반.Items.Add(message);
+                    lst일반.SelectedIndex = lst일반.Items.Count - 1;
+                    break;
+                case Log.실시간:
+                    lst실시간.Items.Add(message);
+                    lst실시간.SelectedIndex = lst실시간.Items.Count - 1;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void axKHOpenAPI_OnReceiveRealCondition(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveRealConditionEvent e)
+        {
+            Logger(Log.실시간, "========= 조건조회 실시간 편입/이탈 ==========");
+            Logger(Log.실시간, "[종목코드] : " + e.sTrCode);
+            Logger(Log.실시간, "[실시간타입] : " + e.strType);
+            Logger(Log.실시간, "[조건명] : " + e.strConditionName);
+            Logger(Log.실시간, "[조건명 인덱스] : " + e.strConditionIndex);
+        }
+
+        private void axKHOpenAPI_OnReceiveTrCondition(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrConditionEvent e)
+        {
+            Logger(Log.조회, "[화면번호] : " + e.sScrNo);
+            Logger(Log.조회, "[종목리스트] : " + e.strCodeList);
+            Logger(Log.조회, "[조건명] : " + e.strConditionName);
+            Logger(Log.조회, "[조건명 인덱스 ] : " + e.nIndex.ToString());
+            Logger(Log.조회, "[연속조회] : " + e.nNext.ToString());
+        }
+
+
+        private void axKHOpenAPI_OnReceiveConditionVer(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveConditionVerEvent e)
+        {
+            if (e.lRet == 1)
+            {
+                Logger(Log.일반, "[이벤트] 조건식 저장 성공");
+            }
+            else
+            {
+                Logger(Log.에러, "[이벤트] 조건식 저장 실패 : " + e.sMsg);
+            }
+
+        }
+
+    }
+}
